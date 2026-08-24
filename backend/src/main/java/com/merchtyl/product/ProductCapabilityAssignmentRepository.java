677f256a2d709f14b527fbc716171af3bb6d61bf
adibhosaleunb <aditya.bhosale@unb.ca>
2026-08-24 12:13:54 -0300

    Fixing unwanted repository bugs

--- backend/src/main/java/com/merchtyl/product/ProductCapabilityAssignmentRepository.java
+++
@@ -1,8 +0,0 @@
-package com.merchtyl.product;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface ProductCapabilityAssignmentRepository extends JpaRepository<ProductCapabilityAssignment, UUID> {
-}
--- backend/src/main/java/com/merchtyl/refunds/RefundItemTaxRepository.java
+++
@@ -1,8 +0,0 @@
-package com.merchtyl.refunds;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface RefundItemTaxRepository extends JpaRepository<RefundItemTax, UUID> {
-}
